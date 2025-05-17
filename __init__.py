61364a77b01974c5f05353ee48932f1ca51f20c8
Max Schäffer <esca@esca-mb-6.local>
2025-05-17 20:27:08 +0200

    Fixes

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+ComfyUI-HunyuanCustom-GGUF - A ComfyUI custom node extension for running Hunyuan models in GGUF format.
+"""
+
+from .comfyui_hunyuancustom_gguf import *
+
+__version__ = "0.1.0"
+__author__ = "Esca / Max Schäffer"
+__email__ = "max@esca-design.de"
+__license__ = "MIT"
+__url__ = "https://github.com/yourusername/ComfyUI-HunyuanCustom-GGUF" 
